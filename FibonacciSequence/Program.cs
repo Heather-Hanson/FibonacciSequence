7e912835c030fbe635c9d13b82b0cb82eae2b537
Heather-Hanson <hjhnsn82@hotmail.com>
2021-05-22 13:07:17 -0300

    Add project files.

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace FibonacciSequence
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.Write("Enter the length of the Fibonacci Series: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", Fibonacci(i));
+            }
+            Console.ReadLine();
+        }
+        public static int Fibonacci(int n)
+        {
+            int firstnumber = 0, secondnumber = 1, result = 0;
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = firstnumber + secondnumber;
+                firstnumber = secondnumber;
+                secondnumber = result;
+            }
+            return result;
+        }
+    }
+
+}
---
+++ FibonacciSequenceTests/ProgramTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FibonacciSequence;
+
+
+namespace FibonacciSequence.Tests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void FibonacciNumber11()
+        {
+            int n = 11;
+            Assert.AreEqual(n, 11);
+
+        }
+        [TestMethod]
+        public void FibonacciNumber0()
+        {
+            int n = 0;
+            Assert.AreEqual(n, 0);
+
+        }
+        [TestMethod]
+        public void FibonacciNumber3()
+        {
+            int n = 3;
+            Assert.AreEqual(n, 3);
+
+        }
+
+    }
+}
+
+
